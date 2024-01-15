a6238f8d069245722eb1638bfa5d14fdc3ef93f9
joehalls605 <46379660+joehalls605@users.noreply.github.com>
2024-01-15 15:33:24 +0000

    app set up

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import navalQuotes from './Quotes/navalQuotes';
 
 function App() {
+  // Since there's no category in navalQuotes, we can directly use the array
+  const items = navalQuotes.map((quote, index) => <p key={index}>{quote.quote}</p>);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-gray-300">
+      <h1>Naval App</h1>
+      <div>{items}</div>
+      {console.log(items)}
     </div>
   );
 }
---
+++ src/Quotes/navalQuotes.js
@@ -0,0 +1,8 @@
+const navalQuotes = [
+    {
+      quote: "Desire is a contract you make with yourself to be unhappy until you get what you want.",
+    }
+  ];
+  
+  export default navalQuotes;
+  
