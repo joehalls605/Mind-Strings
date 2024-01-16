fd23e07ec486adad894c8eea43f3fb42fe0e178e
joehalls605 <46379660+joehalls605@users.noreply.github.com>
2024-01-16 14:35:26 +0000

    modified the name and quotes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import navalQuotes from './Quotes/navalQuotes';
+import quotes from './Quotes/quotes';
 
 function App() {
 
@@ -9,21 +9,25 @@ const handleCategoryChange = (event) =>{
   setSelectedCategory(event.target.value);
 }
 
-  const filteredQuotes = navalQuotes.filter(item => item.category === selectedCategory);
-  const items = filteredQuotes.map((quote, index) => <p key={index}>{quote.quote}</p>);
+  const filteredQuotes = quotes.filter(item => item.category === selectedCategory);
+  const items = filteredQuotes.map((quote, index) => (
+    <p key={index} className="mb-4">{quote.quote} - {quote.author}</p>
+  ));
 
   return (
-    <div className="bg-gray-300">
-      <h1>Naval App</h1>
-      <div>
-        <label htmlFor="category">Select a category:</label>
-        <select id="category" value={selectedCategory} onChange={handleCategoryChange}>
-          <option value="wisdom">wisdom</option>
-          <option value="happiness">happiness</option>
-          <option value="life">life</option>
+    <div className="bg-gray-300  p-5">
+      <h1 className='text-3xl font-bold py-5 font-sans'>Mind Strings</h1>
+      <p className='text-lg font-sans'>Exporting quotes for life and work</p>
+      <div className='pb-5 text-lg font-sans'>
+        <label className="block mb-5 text-lg font-sans" htmlFor="category">Select a category:</label>
+        <select id="category" value={selectedCategory} onChange={handleCategoryChange} >
+        <option value="" disabled hidden>Choose a category</option>
+          <option value="wisdom">Wisdom</option>
+          <option value="happiness">Happiness</option>
+          <option value="life">Life</option>
         </select>
-      </div>
-      <div>{items}</div>
+        </div>
+      <div className='text-lg font-sans py-5'>{items}</div>
       {console.log(items)}
     </div>
   );
