cdf3b2541e8b9e3701b91093f9a0912a5ae3d32e
joehalls605 <46379660+joehalls605@users.noreply.github.com>
2024-01-16 14:49:53 +0000

    quotes update

--- src/App.js
+++ src/App.js
@@ -3,32 +3,32 @@ import quotes from './Quotes/quotes';
 
 function App() {
 
-const [selectedCategory, setSelectedCategory] = useState("wisdom");
+  const [selectedCategory, setSelectedCategory] = useState("wisdom");
 
-const handleCategoryChange = (event) =>{
-  setSelectedCategory(event.target.value);
-}
+  const handleCategoryChange = (event) => {
+    setSelectedCategory(event.target.value);
+  }
 
   const filteredQuotes = quotes.filter(item => item.category === selectedCategory);
   const items = filteredQuotes.map((quote, index) => (
-    <p key={index} className="mb-4">{quote.quote} - {quote.author}</p>
+    <p key={index} className="mb-4 text-2xl font-sans">{quote.quote} - {quote.author}</p>
   ));
 
   return (
-    <div className="bg-gray-300  p-5">
-      <h1 className='text-3xl font-bold py-5 font-sans'>Mind Strings</h1>
-      <p className='text-lg font-sans'>Exporting quotes for life and work</p>
-      <div className='pb-5 text-lg font-sans'>
-        <label className="block mb-5 text-lg font-sans" htmlFor="category">Select a category:</label>
-        <select id="category" value={selectedCategory} onChange={handleCategoryChange} >
-        <option value="" disabled hidden>Choose a category</option>
+    <div className="bg-gray-300 p-5">
+      <h1 className='text-5xl py-5 font-sans'>Mind Strings</h1>
+      <p className='text-2xl font-sans pb-5'>Exporting quotes for life and work</p>
+      <div className='flex items-center pb-5 text-lg font-sans'>
+        <label className="block mr-2 text-xl font-sans" htmlFor="category">Choose a category</label>
+        <select id="category" value={selectedCategory} onChange={handleCategoryChange} className="italic text-2xl">
           <option value="wisdom">Wisdom</option>
           <option value="happiness">Happiness</option>
           <option value="life">Life</option>
         </select>
-        </div>
-      <div className='text-lg font-sans py-5'>{items}</div>
-      {console.log(items)}
+      </div>
+      <div className='mt-40'>
+      <div className=''>{items}</div>
+      </div>
     </div>
   );
 }
--- src/Quotes/quotes.js
+++ src/Quotes/quotes.js
@@ -2,72 +2,72 @@ const quotes = [
   {
     category: 'wisdom',
     author: 'naval',
-    quote: "Desire is a contract you make with yourself to be unhappy until you get what you want.",
+    quote: "Desire is a contract you make with yourself to be unhappy until you get what you want",
   },
   {
     category: 'wisdom',
     author: 'naval',
-    quote: "The goal is not to make money, the goal is to have money.",
+    quote: "The goal is not to make money, the goal is to have money",
   },
   {
     category: 'wisdom',
     author: 'naval',
-    quote: "When you're at peace with yourself and you're driven by internal values, success and failure are just things that happen in the outside world.",
+    quote: "When you're at peace with yourself and you're driven by internal values, success and failure are just things that happen in the outside world",
   },
   {
     category: 'wisdom',
     author: 'naval',
-    quote: "Seek wealth, not money or status. Wealth is having assets that earn while you sleep. Money is how we transfer time and wealth. Status is your place in the social hierarchy.",
+    quote: "Seek wealth, not money or status. Wealth is having assets that earn while you sleep. Money is how we transfer time and wealth. Status is your place in the social hierarchy",
   },
   {
     category: 'wisdom',
     author: 'naval',
-    quote: "The best way to get what you want is to deserve what you want.",
+    quote: "The best way to get what you want is to deserve what you want",
   },
   {
     category: 'wisdom',
     author: 'naval',
-    quote: "A busy mind accelerates the perceived passage of time. Practice slowing down your thoughts.",
+    quote: "A busy mind accelerates the perceived passage of time. Practice slowing down your thoughts",
   },
   {
     category: 'happiness',
     author: 'naval',
-    quote: "Happiness is a choice and a skill and you can devote yourself to learning that skill and making that choice.",
+    quote: "Happiness is a choice and a skill and you can devote yourself to learning that skill and making that choice",
   },
   {
     category: 'happiness',
     author: 'naval',
-    quote: "The foundation of happiness is health – physical, mental, and emotional.",
+    quote: "The foundation of happiness is health – physical, mental, and emotional",
   },
   {
     category: 'happiness',
     author: 'naval',
-    quote: "The trick to being really fulfilled is to be grateful.",
+    quote: "The trick to being really fulfilled is to be grateful",
   },
   {
     category: 'happiness',
     author: 'naval',
-    quote: "Suffering is just a refusal to accept the way things are.",
+    quote: "Suffering is just a refusal to accept the way things are",
   },
   {
     category: 'life',
     author: 'naval',
-    quote: "The time you enjoy wasting is not wasted time.",
+    quote: "The time you enjoy wasting is not wasted time",
   },
   {
     category: 'life',
     author: 'naval',
-    quote: "All the real benefits in life come from compound interest.",
+    quote: "All the real benefits in life come from compound interest",
   },
   {
     category: 'life',
     author: 'naval',
-    quote: "Play long-term games with long-term people.",
+    quote: "Play long-term games with long-term people",
   },
   {
     category: 'life',
     author: 'naval',
-    quote: "Your business and your relationships should be set up so that they can run as well without you as they do with you.",
+    quote: "Your business and your relationships should be set up so that they can run as well without you as they do with you",
   },
 ];
 
